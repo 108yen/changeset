dccdb8623b70fbc597fafd118ea9d867122e5694
108yen <kazuking.1911@gmail.com>
2024-10-10 21:22:50 +0900

    feat: added summary

--- scripts/tag.ts
+++ scripts/tag.ts
@@ -18,55 +18,36 @@ async function main() {
   const major = `v${semver.major(version)}`;
   const minor = `v${semver.major(version)}.${semver.minor(version)}`;
 
-  // const { exitCode, stderr } = await getExecOutput(
-  //   `git`,
-  //   ["ls-remote", "--exit-code", "origin", "--tags", `refs/tags/${tag}`],
-  //   {
-  //     ignoreReturnCode: true,
-  //   }
-  // );
-
-  // if (exitCode === 0) {
-  //   core.setFailed(
-  //     `Action is not being published because version ${tag} is already published`
-  //   );
-  //   return;
-  // }
-
-  // if (exitCode !== 2) {
-  //   core.setFailed(`git ls-remote exited with ${exitCode}:\n${stderr}`);
-  //   return;
-  // }
-
   core.info("Detaching branch....");
   await exec("git", ["checkout", "--detach"]);
-  //MEMO: `git add --force` add all files with ignore .gitignore
-  //so I need to stage force only dist/ directory.
   await exec("git", ["add", "--force", "build"]);
   await exec("git", ["commit", "-m", tag, "--quiet"]);
+
+  core.info(`Push to ${major}...`);
+  await exec("git", ["push", "--force", "origin", `HEAD:refs/heads/${major}`]);
 
   core.info("Creating tags...");
   await exec("git", ["tag", tag]);
   await exec("git", ["tag", minor]);
   await exec("git", ["tag", major]);
 
-  core.info(`Push to ${major}.`);
-  await exec("git", [
-    "push",
-    "--force",
-    "--follow-tags",
-    "origin",
-    `HEAD:refs/heads/${major}`,
-  ]);
-
+  core.info("Push tags...");
   await exec("git", ["push", "--force", "origin", major]);
   await exec("git", ["push", "--force", "origin", minor]);
   await exec("git", ["push", "--force", "origin", tag]);
 
-  //TODO: checkout release branch or no branch
-  //TODO: commit changes
-  //TODO: check existing tags
-  //TODO: update tag or create tag
+  core.summary.addHeading("Generate version tags.", "2");
+  core.summary.addRaw(`Push commit and tags to ${major} branch.`);
+  core.summary.addTable([
+    [
+      { data: "type", header: true },
+      { data: "value", header: true },
+    ],
+    ["semver", tag],
+    ["minor", minor],
+    ["major", major],
+  ]);
+  core.summary.write();
 }
 
 main();
